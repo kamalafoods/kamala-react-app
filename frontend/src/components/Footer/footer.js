a36074ad10fb14fdf2727fb5f5dc76ad909dc833
Raghavendra <raghavredapaka.003@gmail.com>
2026-01-30 15:07:20 +0530

    Updated React components and fixed layout

---
+++ frontend/src/components/Footer/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "10px", background: "#eee", marginTop: "20px" }}>
+      <p>© 2026 Kamala Website</p>
+    </footer>
+  )
+}
